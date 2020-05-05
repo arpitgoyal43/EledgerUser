c3d27f99524c1d96bd69fb313f497e96129bc2d9
“SahilKharya” <kharyasahil@gmail.com>
2020-05-05 16:05:15 +0530

    Variable input Mail at URl

--- UserManagement/src/main/java/in/pune/royforge/eledgerUserData/controller/EmailController.java
+++ UserManagement/src/main/java/in/pune/royforge/eledgerUserData/controller/EmailController.java
@@ -3,24 +3,31 @@ package in.pune.royforge.eledgerUserData.controller;
 import javax.mail.MessagingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.pune.royforge.eledgerUserData.data.service.EmailService;
 
 @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/reset-password")
 public class EmailController {
 
 	@Autowired
 	private EmailService emailService;
 
-	@RequestMapping("/send-mail")
-	public String sendMail() throws MessagingException {
-		String email = "sahilkharya45@gmail.com";
+	@RequestMapping(value = "/{email}", method = RequestMethod.POST)
+	public ResponseEntity<String> sendMail(@PathVariable(value = "email") String email) throws MessagingException {
 		int otp = emailService.generateOTP(email);
-		System.out.print(emailService.getOtp(email));
 
-		return emailService.send(email, "Testing Mail",
-				"Smtp Test Message, OTP is : " + otp + "\n getOTP MEthod() = " + emailService.getOtp(email));
+		return new ResponseEntity<>(
+				emailService.send(email, "Testing Mail",
+						"Smtp Test Message Check, OTP is : " + otp + "/n getOTP MEthod() = " + emailService.getOtp(email)),
+				HttpStatus.CREATED);
 	}
 }
